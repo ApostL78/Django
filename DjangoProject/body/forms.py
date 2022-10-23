943c3aed1a0bbf24d31004543b95f6fb68881f46
ApostL78 <vgwrbrvw@mail.ru>
2022-10-23 14:15:57 +0400

    [upd] forms.py
    Signed-off-by: ApostL78 <vgwrbrvw@mail.ru>

--- DjangoProject/body/forms.py
+++ DjangoProject/body/forms.py
@@ -3,6 +3,7 @@ from .models import Task
 import re
 from django.core.exceptions import ValidationError
 
+
 class TaskForm(forms.ModelForm):
     # title = forms.CharField(label = 'Заголовок' , max_length = 150, widget = forms.TextInput(attrs = {'class': 'form-control'}))
     # task = forms.CharField(label = 'Текст', required = False, widget = forms.Textarea(attrs = {
@@ -17,15 +18,15 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ['title', 'task', 'is_published', 'category']
         widgets = {
-            'title': forms.TextInput(attrs = {'class': 'form-control'}),
-            'task': forms.Textarea(attrs = {'class': 'form-control','rows': 4,}),
-            'category': forms.Select(attrs ={'class': 'form-control'})
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'task': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, }),
+            'category': forms.Select(attrs={'class': 'form-control'})
         }
         empty_labels = {
             'category': 'Выберите категорию'
         }
 
-    def clean_title( self ):
+    def clean_title(self):
         title = self.cleaned_data['title']
         if re.match(r'\d', title):
             raise ValidationError('Цифра в начале')
