a5222311e3e07c2ea9e01ad1c0c3d8f905c80682
ApostL78 <vgwrbrvw@mail.ru>
2022-10-23 13:49:56 +0400

    [upd] urls
    Signed-off-by: ApostL78 <vgwrbrvw@mail.ru>

--- DjangoProject/body/urls.py
+++ DjangoProject/body/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+
 # or from .views import *
 
 urlpatterns = [
     # path('', views.index, name='home'),
-    path('', views.HomeTask.as_view() , name = 'home'),
+    path('', views.HomeTask.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('category/<int:pk>/', views.ViewTask.as_view(), name='category'),
-    path('add-task/', views.CreateTask.as_view(), name = 'add_task'),
+    path('add-task/', views.CreateTask.as_view(), name='add_task'),
 ]
